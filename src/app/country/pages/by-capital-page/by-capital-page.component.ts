157811e67424eff3eab46945683474637fda1641
Tío Gallo <juanftp100@gmail.com>
2025-12-12 15:16:06 -0300

    feat: componente a dividir

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './by-capital-page.component.html'
 })
-export class ByCapitalPageComponent { }
+export class ByCapitalPageComponent {
+
+  onSearch(value: string): void {
+    console.log('Searching for capital:', value);
+  }
+
+}
