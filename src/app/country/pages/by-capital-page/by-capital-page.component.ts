00fc06755e3e95a012b01ffbcf356be2c5cb49a2
Tío Gallo <juanftp100@gmail.com>
2025-12-16 16:43:17 -0300

    uso de resource en vez de la forma clásica de hacer peticiones

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { CountryList } from "../../components/country-list/country-list.component";
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryService } from '../../services/country.service';
-import { Country } from '../../interfaces/country.interface';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -12,33 +12,19 @@ import { Country } from '../../interfaces/country.interface';
 export class ByCapitalPageComponent {
 
   countryService = inject(CountryService);
+  query = signal('');
 
-  isLoading = signal(false);
-  isError = signal<string|null>(null);
-  countries = signal<Country[]>([]);
+  countryResource = resource({
+    request: () => ({ query: this.query() }),
+    loader: async ({ request }) => {
 
-  onSearch(query: string): void {
+      if ( !request.query ) return [];
 
-    if ( this.isLoading() ) return;
+      return await firstValueFrom(
+        this.countryService.searchByCapital( request.query )
+      );
 
-    this.isLoading.set(true);
-    this.isError.set(null);
-
-    this.countryService.searchByCapital(query)
-      .subscribe({
-
-        next: countries => {
-          this.isLoading.set(false);
-          this.countries.set(countries);
-        },
-        error: err => {
-          console.log(err);
-          this.isLoading.set(false);
-          this.countries.set([]);
-          this.isError.set(err);
-        }
-
-      });
-  }
+    },
+  });
 
 }
