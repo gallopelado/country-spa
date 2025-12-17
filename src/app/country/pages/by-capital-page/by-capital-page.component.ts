32522eae89d3c0708df414d44ab4da4f1576c86f
Tío Gallo <juanftp100@gmail.com>
2025-12-17 11:02:54 -0300

    cambio de resources a rxResource

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -2,7 +2,8 @@ import { Component, inject, resource, signal } from '@angular/core';
 import { CountryList } from "../../components/country-list/country-list.component";
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryService } from '../../services/country.service';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, of } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -14,15 +15,13 @@ export class ByCapitalPageComponent {
   countryService = inject(CountryService);
   query = signal('');
 
-  countryResource = resource({
+  countryResource = rxResource({
     request: () => ({ query: this.query() }),
-    loader: async ({ request }) => {
+    loader: ({ request }) => {
 
-      if ( !request.query ) return [];
+      if ( !request.query ) return of([]);
 
-      return await firstValueFrom(
-        this.countryService.searchByCapital( request.query )
-      );
+      return this.countryService.searchByCapital( request.query )
 
     },
   });
--- src/app/country/pages/by-country-page/by-country-page.component.ts
+++ src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, resource, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryList } from "../../components/country-list/country-list.component";
 import { CountryService } from '../../services/country.service';
-import { firstValueFrom } from 'rxjs';
+import { of } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-by-country-page',
@@ -14,6 +15,20 @@ export class ByCountryPageComponent {
   countryService = inject(CountryService);
   query = signal('');
 
+  countryResource = rxResource({
+
+    request: () => ({ query: this.query() }),
+    loader: ({ request }) => {
+
+      if ( !request.query ) return of([]);
+
+      return this.countryService.searchByCountry( request.query )
+
+    },
+
+  });
+
+  /*
   countryResource = resource({
 
     request: () => ({ query: this.query() }),
@@ -28,5 +43,6 @@ export class ByCountryPageComponent {
     },
 
   });
+  */
 
 }
